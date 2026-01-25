8965042369dac8b623f776a1130a02007a2959fe
rahmathidayat553-create <rahmathidayat553@guru.smk.belajar.id>
2026-01-25 16:10:41 +0800

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+export const supabaseUrl = 'https://plovtqealsfydvoonvqn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsb3Z0cWVhbHNmeWR2b29udnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2MzQ5MzMsImV4cCI6MjA4NDIxMDkzM30.-wL6cpOYwBnKdQMTxhNgzFnKwKdXlRx4fjlwan9pVHE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   global: {
